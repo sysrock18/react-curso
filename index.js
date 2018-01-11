1574fe936036c3924f76fe6c02f2d95bad3f56a7
Simon <gonzalezsimon18@gmail.com>
2018-01-10 20:43:50 -0400

    ReactDOM, JSX y componentes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello')
+import React from 'react'
+import { render } from 'react-dom'
+import Media from './src/playlist/components/media'
+
+const container = document.getElementById('app')
+
+const hello = <h1>Hello!</h1>
+
+render(<Media />, app)
---
+++ src/playlist/components/media.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Media extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src="" alt="" width={260} height={160}/>
+          <h3>¿Por que aprender React.js?</h3>
+          <p>Simon Gonzalez</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Media
